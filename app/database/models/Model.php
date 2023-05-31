87245192d506a94db5441d93ead7281a601f46ef
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-05-31 20:54:13 -0300

    "adding user model and general ui for index.php"

--- app/database/models/Model.php
+++ app/database/models/Model.php
@@ -2,18 +2,41 @@
 namespace app\database\models;
 
 use app\database\Connection;
-abstract class Model{
+use PDO;
+abstract class Model
+{
 
   protected string $table;
+  private array $attributes = [];
 
-  public function all(string $fields = '*'){
-    try {
+  public function __set($key, $value)
+  {
+    if (!isset($this->attributes[$key])) {
+      $this->attributes[$key] = $value;
+    }
+  }
+
+  public function __get($key)
+  {
+    return $this->attributes[$key];
+  }
+
+  public function toJson(array $value)
+  {
+    echo json_encode($value);
+  }
+
+  public function all(string $fields = '*')
+  {
+    try 
+    {
       $connection = Connection::getConnection();
       $sql = "select {$fields} from {$this->table}";
       $query = $connection->query($sql);
-      return $query->fetchAll();
+      return $query->fetchAll(PDO::FETCH_CLASS, static::class);
     } 
-    catch (\PDOException $e) {
+    catch (\PDOException $e) 
+    {
       var_dump($e->getMessage());
     }
   }
